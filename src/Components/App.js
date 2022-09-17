dc85fedeea38aa26af5d1a5eebc9628553478612
Victor Vermaas <vhvermaas@gmail.com>
2022-09-17 10:08:49 -0300

    fixing

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,33 +6,15 @@ import AuthSelector from "./AuthSelect.js";
 import SignIn from "./Sign-In/SignIn.js";
 import SignUp from "./Sign-Up/SignUp.js";
 import Home from "./Home/Home.js";
-<<<<<<< HEAD
 import Product from "./Products/Product.js";
-=======
 import Checkout from "./Checkout/Checkout.js";
->>>>>>> main
+
 
 export default function App() {
 
     const [cart, setCart] = useState([])
 
     return(
-<<<<<<< HEAD
-        <>
-            <GlobalStyle />
-            <CartContext.Provider value={{ cart, setCart }}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<AuthSelector />} ></Route>
-                        <Route path="/sign-in" element={<SignIn />} ></Route>
-                        <Route path="/sign-up" element={<SignUp />} ></Route>
-                        <Route path="/home" element={<Home />}></Route>
-                        <Route path="/:productId" element={<Product />}></Route>
-                    </Routes>
-                </BrowserRouter>
-            </CartContext.Provider>
-        </>
-=======
 <>
     <GlobalStyle />
         <CartContext.Provider value={{ cart, setCart }}>
@@ -42,11 +24,12 @@ export default function App() {
                     <Route path="/sign-in" element={<SignIn />} ></Route>
                     <Route path="/sign-up" element={<SignUp />} ></Route>
                     <Route path="/home" element={<Home />}></Route>
+                    <Route path="/:productId" element={<Product />}></Route>
                     <Route path="/checkout" element={<Checkout />}></Route>
                 </Routes>
             </BrowserRouter>
         </CartContext.Provider>
 </>
->>>>>>> main
+
     )
 }
